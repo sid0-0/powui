73ad9689658d6bf18e15e471ad3792e0e72c0965
yodel <43578323+sid0-0@users.noreply.github.com>
2025-07-08 23:08:23 +0530

    add button click animation

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,8 @@ const buttonVariants = cva(
     "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
     "border-3 border-solid border-black text-black shadow-[-6px_6px]",
     "filter-[url(#displacementFilter)]",
-    "spotty-bg-[#eab308] hover:spotty-bg-[#ca8a04]"
+    "spotty-bg-[#eab308] hover:spotty-bg-[#ca8a04]",
+    "transition-[translate,box-shadow] duration-50 linear"
   ),
   {
     variants: {
@@ -45,17 +46,53 @@ function Button({
   variant,
   size,
   asChild = false,
+  ref,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
+    ref?: React.Ref<HTMLButtonElement>;
   }) {
   const Comp = asChild ? Slot : "button";
+
+  const elem = React.useRef<HTMLButtonElement>(null);
+
+  const buttonStateToggle = (state: boolean, elem: HTMLButtonElement) => {
+    console.log("Button ref:", elem);
+    if (!elem) return;
+    if (state) {
+      elem.style.setProperty("translate", "-4px 4px");
+      elem.style.setProperty("box-shadow", "-2px 2px");
+    } else {
+      elem.style.removeProperty("translate");
+      elem.style.removeProperty("box-shadow");
+    }
+  };
+
+  const loadRef = (node: HTMLButtonElement) => {
+    if (!node) return;
+    elem.current = node;
+    elem.current.addEventListener("mousedown", (e) => {
+      buttonStateToggle(true, node);
+    });
+    elem.current.addEventListener("mouseup", (e) => {
+      buttonStateToggle(false, node);
+    });
+    elem.current.addEventListener("mouseleave", (e) => {
+      buttonStateToggle(false, node);
+    });
+    if (ref && typeof ref === "function") {
+      ref(node);
+    } else if (ref && typeof ref === "object") {
+      ref.current = node;
+    }
+  };
 
   return (
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      ref={loadRef}
       {...props}
     />
   );
