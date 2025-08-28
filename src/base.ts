c872b8afa2c230e0e211c812258255c162c08b94
yodel <43578323+sid0-0@users.noreply.github.com>
2025-08-28 12:07:14 +0530

    add a chromatic aberration svg filter

--- src/base.ts
+++ src/base.ts
@@ -21,5 +21,33 @@ export default `
       yChannelSelector="G"
     />
   </filter>
+
+  <filter id="chromaticAberrationFilter">
+      <feComponentTransfer in='SourceGraphic'>
+          <feFuncR type="identity" />
+          <feFuncG type="table" tableValues="0" />
+          <feFuncB type="table" tableValues="0" />
+      </feComponentTransfer>
+      <feOffset dx=-1 dy=1 result="redChannel" />
+
+      <feComponentTransfer in='SourceGraphic'>
+          <feFuncR type="table" tableValues="0 0" />
+          <feFuncG type="identity" />
+          <feFuncB type="table" tableValues="0 0" />
+      </feComponentTransfer>
+      <feOffset dx=1 dy=-1 result="greenChannel" />
+
+      <feComponentTransfer in="SourceGraphic">
+          <feFuncR type="table" tableValues="0 0" />
+          <feFuncG type="table" tableValues="0 0" />
+          <feFuncB type="identity" />
+      </feComponentTransfer>
+
+      <feOffset dx=2 dy=-2 result="blueChannel" />
+
+      <feBlend in="redChannel" mode="lighten" />
+      <feBlend in="greenChannel" mode="lighten" />
+      <feBlend in="blueChannel" mode="lighten" />
+  </filter>
 </svg>
 `;
