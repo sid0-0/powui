efd6f49b641a166d621ff9182b003c9027622985
yodel <43578323+sid0-0@users.noreply.github.com>
2025-09-07 21:53:43 +0530

    fix stories

--- src/stories/Avatar.stories.tsx
+++ src/stories/Avatar.stories.tsx
@@ -11,4 +11,17 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    src: "https://static.dc.com/dc/files/default_images/Char_Thumb_WonderWoman_20190116_5c3fc6aa51d064.76155401.jpg",
+    fallback: "Wonder Woman",
+  },
+};
+
+export const Fallback: Story = {
+  args: {
+    src: "no_image_available",
+    fallback: "Hawley Griffin",
+  },
+};
+
--- src/stories/Avatar.tsx
+++ src/stories/Avatar.tsx
@@ -1,30 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export const StorybookAvatar = () => {
+export const StorybookAvatar = (props: { src: string; fallback?: string }) => {
+  const { src, fallback } = props;
   return (
-    <div className="flex gap-16">
-      <Avatar className="size-24">
-        <AvatarImage src="https://static.dc.com/dc/files/default_images/Char_Thumb_WonderWoman_20190116_5c3fc6aa51d064.76155401.jpg" />
-        <AvatarFallback>Wonder Woman</AvatarFallback>
-      </Avatar>
-      <Avatar className="size-24">
-        <AvatarImage src="no_picture_exists_of_him" />
-        <AvatarFallback>Hawley Griffin</AvatarFallback>
-      </Avatar>
-      <div className="flex -space-x-8">
-        <Avatar className="size-24">
-          <AvatarImage src="https://static.dc.com/dc/files/default_images/Char_Thumb_WonderWoman_20190116_5c3fc6aa51d064.76155401.jpg" />
-          <AvatarFallback>Wonder Woman</AvatarFallback>
-        </Avatar>
-        <Avatar className="size-24">
-          <AvatarImage src="https://static.dc.com/dc/files/default_images/Char_Thumb_Superman_5c3fc2758f6984.90100206.jpg" />
-          <AvatarFallback>Superman</AvatarFallback>
-        </Avatar>
-        <Avatar className="size-24">
-          <AvatarImage src="https://static.dc.com/dc/files/default_images/Char_Thumb_Batman_20190116_5c3fc4b40fae42.85141247.jpg" />
-          <AvatarFallback>Batman</AvatarFallback>
-        </Avatar>
-      </div>
-    </div>
+    <Avatar className="size-24">
+      <AvatarImage src={src} />
+      <AvatarFallback>{fallback}</AvatarFallback>
+    </Avatar>
   );
 };
